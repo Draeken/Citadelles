5341e1456de32b95b88e0c2ee714a4e310d6b238
Draeken <pbm764927@gmail.com>
2020-06-07 18:39:11 +0200

    feat: add card prefabs

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum CardKind
+{
+    A,
+    B,
+    C,
+    D
+}
+
+public class Card : MonoBehaviour
+{    
+    [SerializeField]
+    private int value = 0;
+    [SerializeField]
+    private CardKind kind = CardKind.A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
